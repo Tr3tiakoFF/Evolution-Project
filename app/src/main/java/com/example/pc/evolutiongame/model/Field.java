c903dec09b352c708148d139d300d0038b7f12a5
Yehor <egorken3v@gmail.com>
2019-04-07 19:14:20 +0300

    logic change commit
    (cherry picked from commit 4b8a179a5f584d01331170745cc8fd5a4e7438a0)

--- app/src/main/java/com/example/pc/evolutiongame/model/Field.java
+++ app/src/main/java/com/example/pc/evolutiongame/model/Field.java
@@ -1,5 +1,7 @@
 package com.example.pc.evolutiongame.model;
 
+import com.example.pc.evolutiongame.logic.CubeThrower;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,16 +43,21 @@ public class Field {
         killAllMustNotDie();
     }
 
-    private void killAnimal(int killerNum, int killerPlayerNum, int prayNum, int prayPlayerNum, Room room) {
+    public void killAnimal(int killerNum, int killerPlayerNum, int prayNum, int prayPlayerNum, Room room) {
         prayNum = room.calculateAnimalNum(prayNum, prayPlayerNum);
         killerNum = room.calculateAnimalNum(killerNum, killerPlayerNum);
         if (canKill(killerNum, prayNum)) {
             getAnimals().remove(prayNum);
             getAnimals().get(killerNum).getFood(2);
+            for (int i = 0; i < getAnimalsCount(); i++) {
+                if (getAnimals().get(i).got(LowLevelAnimalProperty.SCAVENGER)) {
+                    getAnimals().get(i).getFood(1);
+                }
+            }
         }
     }
 
-    private void stillFood(int stillerNum, int stillerPlayerNum, int prayNum, int prayPlayerNum, Room room) {
+    public void stillFood(int stillerNum, int stillerPlayerNum, int prayNum, int prayPlayerNum, Room room) {
         prayNum = room.calculateAnimalNum(prayNum, prayPlayerNum);
         stillerNum = room.calculateAnimalNum(stillerNum, prayPlayerNum);
         if (getAnimals().get(prayNum).capacityFood >= 1) {
@@ -98,6 +105,59 @@ public class Field {
         if (pray.got(LowLevelAnimalProperty.TAIL_LOSS)) {
             getAnimals().get(prayNum).removeTailLoss();
             getAnimals().get(killerNum).getFood(1);
+        }
+        if (pray.got(LowLevelAnimalProperty.RUNNING)) {
+            int localeNum = CubeThrower.cubeThrow();
+            System.out.println(localeNum);
+            if (localeNum < 4) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean canKill(Animal animal, Animal animal1) {
+        if (animal1.got(LowLevelAnimalProperty.SWIMMING)) {
+            if (animal.got(LowLevelAnimalProperty.SWIMMING)) {
+            } else {
+                return false;
+            }
+        }
+        if (animal.got(LowLevelAnimalProperty.SWIMMING)) {
+            if (animal1.got(LowLevelAnimalProperty.SWIMMING)) {
+            } else {
+                return false;
+            }
+        }
+        if (animal1.got(LowLevelAnimalProperty.CAMOUFLAGE)) {
+            if (animal.got(LowLevelAnimalProperty.SHARP_VISION)) {
+            } else {
+                return false;
+            }
+        }
+        if (animal1.got(LowLevelAnimalProperty.HIGH_BODY_WEIGHT)) {
+            if (animal.got(LowLevelAnimalProperty.HIGH_BODY_WEIGHT)) {
+            } else {
+                return false;
+            }
+        }
+        if (animal.got(LowLevelAnimalProperty.HIGH_BODY_WEIGHT)) {
+            if (animal1.got(LowLevelAnimalProperty.HIGH_BODY_WEIGHT)) {
+            } else {
+                return false;
+            }
+        }
+        if (animal1.got(LowLevelAnimalProperty.POISONOUS)) {
+        }
+        if (animal1.got(LowLevelAnimalProperty.TAIL_LOSS)) {
+            return false;
+        }
+        if (animal1.got(LowLevelAnimalProperty.RUNNING)) {
+            int localeNum = CubeThrower.cubeThrow();
+            System.out.println(localeNum);
+            if (localeNum < 4) {
+                return false;
+            }
         }
         return true;
     }
