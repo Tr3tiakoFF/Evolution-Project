42905b12daa6770189dad296b6ad2a5d97ae6cf3
dshcherbatiuk <dshcherbatiuk@codeminders.com>
2019-04-22 13:39:24 +0300

    polish code

--- app/src/main/java/com/example/pc/evolutiongame/model/Property.java
+++ app/src/main/java/com/example/pc/evolutiongame/model/Property.java
@@ -10,4 +10,11 @@ public class Property {
     public LowLevelAnimalProperty getValue() {
         return value;
     }
+
+    @Override
+    public String toString() {
+        return "Property{" +
+                "value=" + value +
+                '}';
+    }
 }
